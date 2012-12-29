c8ebc3c52645340aca125f0c76b4cca2eece884a
Landon Kuhn <landon9720@gmail.com>
2012-12-28 19:18:47 -0800

    sigh

---
+++ project/OthellogeneticevolutionBuild.scala
@@ -0,0 +1,18 @@
+import sbt._
+import sbt.Keys._
+
+object OthellogeneticevolutionBuild extends Build {
+
+  lazy val othellogeneticevolution = Project(
+    id = "othello-genetic-evolution",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "othello-genetic-evolution",
+      organization := "kuhn",
+      version := "0.1-SNAPSHOT",
+      scalaVersion := "2.9.2",
+      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+      libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.0.1"
+    )
+  )
+}
