ae505b5731848cb02720a29f45399fe86dcb6b99
landon9720 <landon9720@gmail.com>
2013-01-01 18:37:44 -0800

    renamed that

--- src/main/scala/kb/strategy.scala
+++ src/main/scala/kb/strategy.scala
@@ -1,19 +1,19 @@
 package kb
 
 trait Strategy {
-	def ratePlay(c:Color, col:Int, row:Int, board:Board):Boolean
+	def rate(c:Color, col:Int, row:Int, board:Board):Boolean
 }
 
 object Strategy {
 	class Corner extends Strategy {
-		def ratePlay(c: Color, col: Int, row: Int, board: Board) =
+		def rate(c: Color, col: Int, row: Int, board: Board) =
 			(col == 0 && row == 0) ||
 			(col == 0 && row == 7) ||
 			(col == 7 && row == 0) ||
 			(col == 7 && row == 7)
 	}
 	class Edge extends Strategy {
-		def ratePlay(c: Color, col: Int, row: Int, board: Board) =
+		def rate(c: Color, col: Int, row: Int, board: Board) =
 			((row == 0 || row == 7) && col >= 2 && col <= 5) ||
 			((col == 0 || col == 7) && row >= 2 && row <= 5)
 	}
